415b0e4d2e41b708e5ed7b8baba4a4fb2130db6f
MoonlightEnjoyer <artiom.labietskii@gmail.com>
2023-09-24 13:58:46 +0300

    Stable

--- LogFilter/filecontext.cpp
+++ LogFilter/filecontext.cpp
@@ -16,6 +16,7 @@
 #include <QtConcurrent/QtConcurrent>
 #include <QFile>
 #include <re2/re2.h>
+#include <absl/strings/string_view.h>
 
 void FileContext::search()
 {
@@ -41,39 +42,26 @@ void FileContext::processFile(int id, int threadsNumber, FileContext* currentCon
         bytesToProcess = (mapped_size - mapped_size % threadsNumber) / threadsNumber;
     }
 
-    //char* stringToAnalize = new char[30000];
     long matchCounter = 0;
     long memoryStart = id * bytesToProcess;
     long memoryEnd = (id + 1) * bytesToProcess;
     uchar* memoryWriteStart = memoryStart + mapped_result_file;
-    uchar* memoryReadStart;
-    uchar* memoryReadEnd;
 
-    for (long i = id * bytesToProcess; i < memoryEnd; i++)
+    for (long i = memoryStart; i < memoryEnd; i++)
     {
         for (long j = i; j < memoryEnd; j++)
         {
             if (mapped_file[j] == '\n')
             {
-                memoryReadStart = mapped_file + i;
-                memoryReadEnd = mapped_file + j;
+                std::copy(mapped_file + i, mapped_file + j, memoryWriteStart + matchCounter);
 
-                //std::copy(memoryReadStart, memoryReadEnd, stringToAnalize);
-                std::copy(memoryReadStart, memoryReadEnd, memoryWriteStart + matchCounter);
-
-                if (RE2::PartialMatch((char*)(memoryWriteStart + matchCounter), filter_regex, (void*)NULL))
-                {
-                    //std::copy(memoryReadStart, memoryReadEnd, memoryWriteStart + matchCounter);
-                    matchCounter += j - i;
-                }
+                matchCounter += (j - i) * RE2::PartialMatch((char*)(memoryWriteStart + matchCounter), filter_regex, (void*)NULL);
 
                 i = j;
                 break;
             }
         }
     }
-
-    //delete [] stringToAnalize;
 }
 
 void FileContext::startThreads(FileContext* currentContext, uchar* mapped_file, uchar* mapped_result_file, long mapped_size)
